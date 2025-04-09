eceeac622d9fdd4471f7c27dd21d453354ca79b2
moeidkh <moeid.kh1381@gmail.com>
2025-04-10 00:44:49 +0330

    fix: Adds loading for background video

--- src/Redux/Background/BackgroundReducer.ts
+++ src/Redux/Background/BackgroundReducer.ts
@@ -1,16 +1,35 @@
-const initialState = {
-    backgroundUrl: "/video/video1.mp4"
-}
+type InitialStateType = {
+  backgroundUrl?: string;
+  loading?: boolean;
+};
+const ActionType = {
+  SetLoading: "Background/SET_LOADING",
+  SetBackground: "Background/SET_BACKGROUND",
+};
 
-const backgroundReducer = (state=initialState, action) => {
-    switch(action.type){
-        case "SETBACKGROUND":
-            return {
-                backgroundUrl: action.bg
-            }
-        default:
-            return state
-    }
-}
+const initialState: InitialStateType = {
+  backgroundUrl: "/video/video1.mp4",
+  loading: true,
+};
+
+const backgroundReducer = (
+  state = initialState,
+  action: { type: keyof typeof ActionType; payload: InitialStateType }
+) => {
+  switch (action.type) {
+    case ActionType.SetBackground:
+      return {
+        ...state,
+        backgroundUrl: action.payload.backgroundUrl,
+      };
+    case ActionType.SetLoading:
+      return {
+        ...state,
+        loading: action.payload.loading,
+      };
+    default:
+      return state;
+  }
+};
 
 export default backgroundReducer;
